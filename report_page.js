1d6f96f7a70ab6015c05db16e1bf9907963c0335
SammyCat222 <Samantha.Cotugno001@umb.edu>
2026-05-05 16:46:13 -0400

    Update report_page.js
    Report_page.js now successfully collects the form data to an array. Connection to offending user and content is in the testing phase.

--- report_page.js
+++ report_page.js
@@ -1,18 +1,38 @@
-function submit() {
 
+ function newsubmit() {
 
-        const selected = document.querySelector("input[name='rules']:checked");
+        const r = document.getElementById('report');
+
+        const selected = document.querySelector('input[name="rules"]:checked');
+
         const val = selected ? selected.value : null;
 
+        const form = [];
+        console.log(val);
 
         const desc = document.getElementById("other").value;
+        console.log(desc);
 
-        const reportdata = {
-        rule: val,
+        const  allResponses = JSON.parse(localStorage.getItem('allResponses')) || [];
+
+        const user = allRepsonses.dataset.author;
+
+        const side = allResponses.dataset.side;
+
+        const reason = allResponses.dataset.reason;
+
+        const reportData = {
+        rules: val,
         description: desc,
-        time: new Date().toISOString()
+        timestamp: new Date().toISOString()
+
+        username: user,
+        sidetaken: side,
+        reasoning: reason
         };
 
-        localStorage.setItem("reports", JSON.stringify(reportdata));
+        localStorage.setItem("reports", JSON.stringify(reportData));
+        window.location.href= "https://www.cs.umb.edu/~hdeblois/cs410/longproj01/t1-UP/Artifacts/samanthaArtifacts/report_page/report_review.html";
+
 
  }
